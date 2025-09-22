d5e9564dbdc2635933e92663dbb1b282d3e18a08
Kashif Ali <malikkashif5035@gmail.com>
2025-09-22 16:34:35 +0500

    Update utils.js

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -6,9 +6,9 @@ export const generateToken = (userId, res) => {
   });
   res.cookie("jwt", token, {
     maxAge: 30 * 24 * 60 * 60 * 1000,
-    httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-    sameSite: "strict", // CSRF attacks cross-site request forgery attacks
-    secure: process.env.NODE_ENV !== "development", // prevent man-in-the-middle (MITM) attacks.
+    httpOnly: true,
+    sameSite: "strict",
+    secure: process.env.NODE_ENV !== "development",
   });
 
   return token;
